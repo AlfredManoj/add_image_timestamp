2660d8da5ccb24c4e88ed873f503b0c84dc3de1e
Alfred Manoj <alfredmanoj@users.noreply.github.com>
2024-04-25 00:19:16 -0400

    added source code - initial version - hardcoded cases

---
+++ image_timestamp.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+import os
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        imag = cv2.imread(os.path.join(folder, filename))
+        if imag is not None:
+            images.append(filename)
+    return images
+
+
+image_folder_path = os.path.join(os.getcwd(), "Images")
+result_folder_path = os.path.join(os.getcwd(), "Result")
+
+image_list = load_images_from_folder(image_folder_path)
+for image in image_list:
+    # Open an Image
+    img = Image.open(os.path.join(os.getcwd(), "Images", image))
+
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
+
+    # Custom font style and font size
+    myFont = ImageFont.truetype('Roboto-Regular.ttf', 20)
+
+    (x, y) = img.size
+
+    # Add Text to an image
+    I1.text((10, y-35), "2024/4/25  09:41", font=myFont, fill=(255, 255, 255))
+
+    # Display edited image
+    # img.show()
+
+    # Save the edited image
+    img.save(os.path.join(os.getcwd(), "Result", image))
+
